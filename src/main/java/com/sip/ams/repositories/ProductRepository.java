d2fc83fdc5505074d865c65929e208376ebfa9b6
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-05-02 14:17:46 +0100

    Unit Test avec Spring Data

--- src/main/java/com/sip/ams/repositories/ProductRepository.java
+++ src/main/java/com/sip/ams/repositories/ProductRepository.java
@@ -2,9 +2,11 @@ package com.sip.ams.repositories;
 
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sip.ams.entities.Product;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
      
     public Product findByName(String name);
